fb58afa8e42a70852035b1f0df6b7452b53a64bc
jdelacruzv <jldlcv@gmail.com>
2020-12-07 13:39:26 -0500

    Segundo avance del proyecto

--- memory.js
+++ memory.js
@@ -11,48 +11,30 @@ closewindow.addEventListener("click", () => container.classList.remove("show"))
 loadPage = () => location.reload();
 
 
-// Tiempo restante
-startday = new Date();
+// Tiempo acumulado(temporizador)
+// setInterval(): sirve para ejecutar una función infinitamente cada x milisegundos.
+// setTimeout(): ejecuta una función una sola vez después de x milisegundos. 
 
-initStopwatch = () => {
-    var myTime = new Date();
-    return (myTime.getTime() - startday.getTime()) / 1000;
+var second = 1, minute = 0, hour = 0;
+var capture = document.getElementById("timer");
+
+printTimer = () => {
+    return `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second : second}`;
 }
 
-calculateTime = () => { 
-    var tSecs = Math.floor(initStopwatch());
-    var second = tSecs % 60;
-    second = "" + (second < 10 ? "0" + second : second);
-
-    var minute = Math.floor((tSecs / 60) % 60);
-    minute = "" + (minute < 10 ? "0" + minute : minute);
-    
-    var hour = Math.floor(tSecs / 3600)    
-    hour = "" + (hour < 10 ? "0" + hour : hour);
-
-    document.getElementById('timeleft').value = hour + ":" + minute + ":" + second;
-    window.setTimeout("calculateTime()", 1000); // Actualiza cada segundo
+calculateTimer = () => {
+    setInterval(() => { 
+            capture.innerHTML = printTimer();
+            second++;
+            if(second === 59) {
+                second = 0;
+                minute++;
+                if(minute === 59) {
+                    minute = 0;
+                    hour++;
+                }
+            }
+    }, 1000);
 }
-
-window.onload = () => window.setTimeout("calculateTime()", 1);
-
-// startday = new Date();
-// clockStart = startday.getTime(); // Devuelve el número en milisegundos
-
-// initStopwatch = () => {
-//     var myTime = new Date();
-//     return ((myTime.getTime() - clockStart) / 1000);
-// }
-
-// getSecs = () => { 
-//     var tSecs = Math.round(initStopwatch());
-//     var iSecs = tSecs%60;
-//     var iMins = Math.round((tSecs-30)/60);
-//     var sSecs = "" + ((iSecs>9) ? iSecs : "0" + iSecs);
-//     var sMins = "" + ((iMins>9) ? iMins : "0" + iMins);
-//     document.getElementById('timespent').value = sMins + ":" + sSecs;
-//     // Actualiza cada segundo
-//     window.setTimeout('getSecs()', 1000);
-// }
-
-// window.onload = () => window.setTimeout('getSeconds()', 1);
+// Carga el contador a la pagina y espera un segundo para arrancar
+window.onload = () => window.setTimeout('calculateTimer()', 1);
